969280c7267e7b81862306a70d1f5c1af77298e6
Anttoni Maula <manttoni92@gmail.com>
2024-10-04 23:10:15 +0300

    this way is much more workable

--- src/cmd_split.c
+++ src/cmd_split.c
@@ -1,101 +1,76 @@
 #include "minishell.h"
 
-char	*get_arg(char *cmd)
+char	*append(char *beginning, char *end)
+{
+	char	*chain;
+	int		blen;
+	int		elen;
+	int		i;
+
+	blen = ft_strlen(beginning);
+	elen = ft_strlen(end);
+	chain = malloc((blen + elen + 1));
+	i = 0;
+	while (i < blen)
+	{
+		chain[i] = beginning[i];
+		i++;
+	}
+	while (i < blen + elen)
+	{
+		chain[i] = end[i - blen];
+		i++;
+	}
+	chain[i] = '\0';
+	free(beginning);
+	free(end);
+	return (chain);
+}
+
+int	get_arg(char **arg, char *cmd, char **env)
 {
 	char	inside;
 	char	*ptr;
+	char	*result;
+	char	*chrstr;
 
 	inside = 0;
 	ptr = cmd;
+	result = malloc(1);
+	result[0] = '\0';
 	while (*ptr)
 	{
 		if (*ptr == inside) // quote closes
 		{
 			inside = 0;
-			ptr++;
-			continue ;
+			ft_memmove(ptr, ptr + 1, ft_strlen(ptr + 1)); // hide quote
 		}
 		if (*ptr == '\'' || *ptr == '\"') // quote opens
+		{
 			inside = *ptr;
-		if (*ptr == ' ' && inside == 0)
-			return (ft_substr(cmd, 0, ptr - cmd));
+			ft_memmove(ptr, ptr + 1, ft_strlen(ptr + 1));
+		}
+		if (*ptr == ' ' && inside == 0) // space that is not in quotes
+		{
+			*arg = result;
+			return (ptr - cmd);
+		}
+		if (*ptr == '$' && inside != '\'') // dollar that is not in single quotes
+		{
+			result = append(result, ft_getenv(get_key(ptr), env));
+			ptr += ft_strlen(get_key(ptr));
+		}
+		else if (*ptr != inside)
+		{
+			chrstr = malloc(2);
+			chrstr[0] = *ptr;
+			chrstr[1] = '\0';
+			result = append(result, chrstr);
+		}
 		ptr++;
 	}
-	return (ft_strdup(cmd));
-}
-
-char	*replace_dollar(char *arg, char *dollar, char **env)
-{
-	char	*key;
-	char	*value;
-	char	*new;
-	int		len;
-	char	*end_part;
-
-	key = get_key(dollar);
-	value = ft_getenv(key, env);
-	len = ft_strlen(arg) - ft_strlen(key) - 1;
-	len += ft_strlen(value) + 1;
-	new = malloc(len * sizeof(char));
-	new = ft_substr(arg, 0, dollar - arg);
-	new = ft_strjoin(new, value);
-	end_part = ft_strdup(arg + (dollar - arg) + 1 + ft_strlen(key));
-	new = ft_strjoin(new, end_part);
-	free(key);
-	free(value);
-	free(arg);
-	return (new);
-}
-
-char	*expand(char *arg, int qstart, int qend, char **env)
-{
-	char	*dollar;
-
-	dollar = ft_strrchr(arg, '$');
-	while (dollar)
-	{
-		if (dollar - arg >= qstart && dollar - arg < qend)
-		{
-			arg = replace_dollar(arg, dollar, env);
-			//ft_memmove(dollar, dollar + 1, ft_strlen(dollar + 1));
-		}
-		dollar = ft_strrchr(arg, '$');
-	}
-	return (arg);
-}
-
-char	*format(char *arg, char **env)
-{
-	char	quote;
-	char	*end_quote;
-	int		i;
-
-	i = 0;
-	quote = 0;
-	while (arg[i])
-	{
-		if (arg[i] == '\'' || arg[i] == '\"')
-		{
-			quote = arg[i];
-			ft_memmove(&arg[i], &arg[i + 1], ft_strlen(&arg[i + 1]));
-			break ;
-		}
-		i++;
-	}
-	if (quote != 0)
-	{
-		end_quote = ft_strrchr(arg, quote);
-		ft_memmove(end_quote, end_quote + 1, ft_strlen(end_quote + 1));
-		arg[ft_strlen(arg) - 2] = '\0';
-	}
-	else
-	{
-		i = 0;
-		end_quote = &arg[ft_strlen(arg)];
-	}
-	if (quote != '\'')
-		arg = expand(arg, i, end_quote - arg, env);
-	return (arg);
+	*arg = result;
+	return (ft_strlen(cmd));
 }
 
 char	**cmd_split(char *cmd, char **env)
@@ -112,9 +87,8 @@ char	**cmd_split(char *cmd, char **env)
 			cmd++;
 		if (*cmd == '\0')
 			break ;
-		arg = get_arg(cmd);
-		len = ft_strlen(arg);
-		arg = format(arg, env);
+		len = get_arg(&arg, cmd, env);
+		printf("arg = %s\n", arg);
 		ar = add(ar, arg);
 		cmd += len;
 	}
