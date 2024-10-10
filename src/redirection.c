d0984a782430fab7e398ad880ac0a80a5fedbce5
Anttoni Maula <amaula@c1r5p11.hive.fi>
2024-10-10 12:53:11 +0300

    redirection.c frees memory and is simpler

--- src/redirection.c
+++ src/redirection.c
@@ -10,6 +10,43 @@ int	filename_len(char *cmd)
 	return (len);
 }
 
+/*char dir is << or >> */
+char	**get_fileinfo(char *cmd, char *dir)
+{
+	char	**file_info;
+
+	if (ft_strchr(cmd, *dir) == NULL)
+		return (NULL);
+	file_info = malloc(2 * sizeof(char *));
+	memset(file_info, 0, 2 * sizeof(char *));
+	while (*cmd)
+	{
+		if (*cmd == *dir) // one dir char
+		{
+			cmd++;
+			free(file_info[1]);
+			if (*cmd == *dir) // two dir chars
+			{
+				file_info[1] = ft_strdup(dir);
+				cmd++;
+			}
+			else
+				file_info[1] = ft_substr(dir, 0, 1);
+			if (*cmd == ' ')
+				cmd++;
+			if (filename_len(cmd) > 0)
+				file_info[0] = ft_substr(cmd, 0, filename_len(cmd));
+			else
+			{
+				printf("Syntax error\n");
+				return (NULL);
+			}
+		}
+		cmd++;
+	}
+	return (file_info);
+}
+/*
 // returns the last "<" or "<<" + filename/delimiter
 char	**extract_filein(char *cmd)
 {
@@ -66,4 +103,4 @@ char	**extract_fileout(char *cmd)
 		cmd++;
 	}
 	return (file_info);
-}
+}*/
