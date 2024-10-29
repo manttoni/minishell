139c7bc439e6e42dadf6485b1257dc8e63bcea48
607mark <40577872+607mark@users.noreply.github.com>
2024-10-29 22:14:52 +0200

    ./ are only for current dir

--- src/path.c
+++ src/path.c
@@ -66,8 +66,14 @@ char *find_path(t_command *cmd, t_env *env)
 	char *path_var;
 	char *result;
 	char **path;
-	if (access(cmd->args[0], F_OK | X_OK) == 0)
+	
+	if (access(cmd->args[0], F_OK | X_OK) == 0 && !ft_strncmp(cmd->args[0], "./", 2))
 		return (cmd->args[0]);
+	else if (access(cmd->args[0], F_OK | X_OK) && !ft_strncmp(cmd->args[0], "./", 2))
+	{	
+		print_error(cmd->args[0], 0);
+		return (print_error(": command not found", 2));
+	}
 	path_var = ft_getenv("PATH", env);
 	if (!path_var)
 	{	
