3e916982f9b91ece50e719129d8ed6802e635274
Anttoni Maula <manttoni92@gmail.com>
2024-11-08 07:51:04 +0200

    builtin tester

--- src/minishell.c
+++ src/minishell.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv, char **env)
 
 	if (argc != 1)
 	{
-		printf("Too many arguments\n");
+		printf("Too many arguments (%d)\n", argc);
 		return (1);
 	}
 	(void) argv;
