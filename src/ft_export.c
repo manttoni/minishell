ec08f8f15071686ec68424547bce767e9cf9c92c
Anttoni Maula <manttoni92@gmail.com>
2024-11-22 12:45:33 +0200

    fixed double error

--- src/ft_export.c
+++ src/ft_export.c
@@ -6,13 +6,13 @@
 /*   By: amaula <amaula@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:35:09 by amaula            #+#    #+#             */
-/*   Updated: 2024/11/20 16:35:10 by amaula           ###   ########.fr       */
+/*   Updated: 2024/11/22 12:44:48 by amaula           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	validate_arg(char *arg)
+static int	invalid_arg(char *arg)
 {
 	char	**key_value;
 
@@ -21,7 +21,6 @@ int	validate_arg(char *arg)
 	key_value = ft_split(arg, '=');
 	if (is_valid_key(key_value[0]) == 0)
 	{
-		print_error(ft_strjoin(arg, ": invalid export"), 3);
 		free_array(key_value);
 		return (1);
 	}
@@ -30,7 +29,9 @@ int	validate_arg(char *arg)
 }
 
 /* Adds or updates the env variables specified by args
- * if it already exists, it first gets removed */
+ * if it already exists, it first gets removed 
+ * If one of the export args is invalid, return will be 1 
+ * 0 if all are valid */
 int	ft_export(char **args, t_env *env)
 {
 	char	*buf[2];
@@ -40,7 +41,7 @@ int	ft_export(char **args, t_env *env)
 	buf[1] = NULL;
 	while (*args)
 	{
-		ret = validate_arg(*args);
+		ret = invalid_arg(*args);
 		if (ret != 0)
 		{
 			print_error(ft_strjoin(*args, ": invalid export"), 3);
